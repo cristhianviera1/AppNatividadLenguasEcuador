c0b3fa1e1613692a8c8647b33b3fcb5227441b93
cristhian.viera1 <cristhian.viera1@gmail.com>
2019-07-31 22:46:33 -0500

    Se agrega la data a la aplicación para abrir localmente

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,11 +9,13 @@ import * as L from 'leaflet';
 })
 export class HomePage {
   map: L.Map;
-  //json : any;
+  lenguas: Array<string>
+  comunidades: Array<any>
   constructor(private http: HttpClient) { }
   ionViewDidEnter() {
     this.loadmap();
     this.getEcuadorShape();
+    this.getComunidadesShape();
   }
 
   loadmap() {
@@ -22,23 +24,40 @@ export class HomePage {
       // tslint:disable-next-line
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18
-    }).addTo(this.map);    
+    }).addTo(this.map);
   }
-  getEcuadorShape(){
+  getEcuadorShape() {
     this.http.get('assets/shapeFiles/ecuador.json').subscribe((json: any) => {
-      var ecuador = L.geoJSON(json,{
-        onEachFeature: function(feature, layer){
-          layer.on('click',function(e){
+      var ecuador = L.geoJSON(json, {
+        onEachFeature: function (feature, layer) {
+          layer.on('click', function (e) {
             //Do something when click on any province
           })
         },
         //change style of provinces
         style: function (layer) {
-        return { fillOpacity: 0.3, color: '#055', weight: 1 }
-    }
+          return { fillOpacity: 0.3, color: '#055', weight: 1 }
+        }
       }).addTo(this.map);
       this.map.fitBounds(ecuador.getBounds());
     });
+  }
+  getComunidadesShape() {
+    this.http.get('assets/shapeFiles/ddbb.json').subscribe((json: any) => {
+      L.geoJson(json, {
+        onEachFeature: function (feature) {
+          //.push(feature);
+          //console.log(this.comunidades)
+          /*
+        if (this.lenguas.indexOf(feature.properties.LENGUA_L1) === -1) {
+            this.lenguas.push(feature.properties.LENGUA_L1)
+        }*/
+        },
+        style: function (layer) {
+          return { fillOpacity: 0.8, color: '#555' }
+        }
+      })
+    })
   }
 
 }
