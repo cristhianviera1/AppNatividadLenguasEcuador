69fa412339746f7b865fa7c4200815d3eb739b67
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2019-08-01 00:06:04 -0500

    culturas

---
+++ src/app/cultura-descripcion/cultura-descripcion.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cultura-descripcion',
+  templateUrl: './cultura-descripcion.page.html',
+  styleUrls: ['./cultura-descripcion.page.scss'],
+})
+export class CulturaDescripcionPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
