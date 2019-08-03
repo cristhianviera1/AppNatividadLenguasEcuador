e9bafa44ce2255b7eb0734cab9e6d4363e571ff6
JosseAguirre <jac.aguirre@yavirac.edu.ec>
2019-08-03 12:54:49 -0500

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,15 @@
 
 export const environment = {
   production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCzi2LrvFnUYo_oKhwcBGsucht_DfKc0nM",
+    authDomain: "natividad-de-lenguas.firebaseapp.com",
+    databaseURL: "https://natividad-de-lenguas.firebaseio.com",
+    projectId: "natividad-de-lenguas",
+    storageBucket: "",
+    messagingSenderId: "979706024262",
+    appId: "1:979706024262:web:027f515963d9ee7b"
+  },
   comunidadesPath: "http://www.google.com",
   API_URL: 'http://192.168.100.149:8000'
 };
